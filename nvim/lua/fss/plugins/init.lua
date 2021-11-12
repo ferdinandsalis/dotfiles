907fb1546d75eb77a683efe753d803cca6d006f2
Ferdinand Salis-Samaden <mail@ferdinandsalis.com>
2021-11-12 12:43:42 +0100

    feat(copilot): enable for all but commit message
    and add key mapping to explicitly accept copilot suggestion

--- nvim/lua/fss/plugins/init.lua
+++ nvim/lua/fss/plugins/init.lua
@@ -845,13 +845,11 @@ require('packer').startup {
       'github/copilot.vim',
       config = function()
         vim.g.copilot_filetypes = {
-          ['*'] = false,
-          lua = true,
-          javascript = true,
-          javascriptreact = true,
-          typescript = true,
-          typescriptreact = true,
+          ['*'] = true,
+          gitcommit = false,
+          NeogitCommitMessage = false
         }
+        fss.imap('<c-h>', [[copilot#Accept("\<CR>")]], { expr = true, script = true })
         vim.g.copilot_no_tab_map = true
         vim.g.copilot_assume_mapped = true
         vim.g.copilot_tab_fallback = ''
