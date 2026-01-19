ed72854fac9aeaac83deacf7fa581ec43aebf8f0
Ferdinand Salis-Samaden <mail@ferdinandsalis.com>
2026-01-19 15:13:23 +0100

    style(toggle-theme): remove trailing whitespace

--- scripts/toggle-theme.sh
+++ scripts/toggle-theme.sh
@@ -24,15 +24,15 @@ if [ "$CURRENT_THEME" = "mocha" ]; then
 else
     NEW_THEME="mocha"
     NEW_THEME_FULL="catppuccin-mocha"
-    
+
     # FZF colors for Mocha theme
     FZF_COLORS="--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
-    
+
     # Helix theme
     HELIX_THEME="catppuccin_mocha"
-    
+
     # Bat theme
     BAT_THEME="Catppuccin-mocha"
 fi
